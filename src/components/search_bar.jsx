6f1c73654219b83d85c375a0d14b52f4857adb78
Quan Nguyen <quanguyen250@gmail.com>
2019-12-21 19:00:07 -0500

    Finished first react project

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -8,6 +8,7 @@ class SearchBar extends Component {
   render () {
     return(
       <input type="text"
+      placeholder="Type something here"
       className="form-control form-search"
       onChange={this.handleUpdate}
       />
